1e899915eb89feaab025ae848cadef16bacc8a5e
webancosur-oss <web.ancosur@gmail.com>
2026-08-25 10:14:07 -0500

    fix: remove unused CabinsSection and AmenitiesSection imports from HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
 
 
-import AmenitiesSection from "./components/AmenitiesSection/AmenitiesSection";
-import CabinsSection from "./components/CabinsSection/CabinsSection";
 import FaqSection from "./components/Faq/FaqSection";
-import FinalCta from "./components/FinalCta/FinalCta";
 import CinematicExperience from "./components/home/CinematicExperience/CinematicExperience";
 import HeroSection from "./components/home/HeroSection";
 import HomeContactForm from "./components/home/HomeContactForm";
-import ElementsShowcase from "./components/home/HomeHero";
-import HomeHero from "./components/home/HomeHero";
 import LocationSection from "./components/LocationSection/LocationSection";
 import LotsSection from "./components/LotsSection/LotsSection";
 import ZagariManifesto from "./components/Manifesto/ZagariManifesto";
-import Navbar from "./components/navbar/Navbar";
-import NearbyExperiences from "./components/NearbyExperiences/NearbyExperiences";
 import ZagariExperience from "./components/ZagariExperience/ZagariExperience";
 import styles from "./page.module.css";
 
